f519b8c3c4a0ac64df36724ace1f275a319514b3
OtabekTohirovich <tohirovo630@gmail.com>
2023-08-15 18:38:32 +0500

    Addation some changes and modal

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,30 +1,32 @@
 "use client";
 import { ProductType } from "@/interfaces";
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
+import CustumImage from "./image";
 
 const Product: FC<{ product: ProductType }> = ({ product }) => {
   console.log(product);
 
   return (
-    <div>
-      <div className="bg-gray-100 p-6 rounded-lg">
-        <img
-          className="h-40 rounded w-full object-cover object-center mb-6"
-          src="https://dummyimage.com/720x400"
-          alt="content"
-        />
-        <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-          {product.category}
-        </h3>
-        <div className="font-semibold flex items-center justify-between mt-4 mb-1">
-          <p className="w-44 truncate">{product.title}</p>
-          <p>${product.price}</p>
-        </div>
-        <p className="leading-relaxed text-base line-clamp-2">
-          {product.description}
-        </p>
+    <Link
+      href={`/product/${product.id}`}
+      className=" border flex flex-col h-96 p-6 rounded-lg group hover:scale-105 transition-transform ease-out duration-200"
+    >
+      <div className="relative max-h-80 flex-1">
+        <CustumImage product={product} fill />
       </div>
-    </div>
+      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+        {product.category}
+      </h3>
+      <div className="font-semibold flex items-center justify-between mt-4 mb-1">
+        <p className="w-44 truncate">{product.title}</p>
+        <p>${product.price}</p>
+      </div>
+      <p className="leading-relaxed text-base line-clamp-2">
+        {product.description}
+      </p>
+    </Link>
   );
 };
 
